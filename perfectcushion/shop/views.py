a0864d927ea043baf0e8d1add3aee387906745c4
문형권 <sysmoon@gmail.com>
2019-02-08 15:45:12 +0900

    devops test1

--- perfectcushion/shop/views.py
+++ perfectcushion/shop/views.py
@@ -4,7 +4,7 @@ from .models import Category, Product
 
 # Create your views here.
 def index(request):
-    text_var='this is my first django app web'
+    text_var='this is my first django app web test1'
     return HttpResponse(text_var)
 
 # category view
