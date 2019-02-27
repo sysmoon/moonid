b521ae589296ea9da79bb3d52a79d016819f3582
Daniel Moon <sysmoon@syshome.local>
2019-02-27 21:22:11 +0900

    replace sqlite with azure mariadb

--- Application/perfectcushion/perfectcushion/settings.py
+++ Application/perfectcushion/perfectcushion/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'perfectcushion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'moonid',
+    'USER': 'sysmoon@mooid-maria-db',
+    'PASSWORD': 'qwerqwer4712!',
+    'HOST': 'mooid-maria-db.mariadb.database.azure.com',   
+    'PORT': '3306',
     }
 }
 
